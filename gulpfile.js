877aa2bca53dfa00bc15e3da3319a0b6f0d5bf51
David R. Poindexter III <drpoindexter@gmail.com>
2015-02-06 18:04:25 -0500

    Add initial tool infrastructure

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+ * Super Task Runner
+ */
+
+(function () {
+    'use strict';
+
+    var gulp = require('gulp');
+
+    gulp.task('default', function () {
+        console.log('gulp enabled');
+    })
+
+})();
