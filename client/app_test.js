837970ee35dbca5b10fcf18f9fb04d17138c82d4
David R. Poindexter III <drpoindexter@gmail.com>
2015-02-06 21:57:18 -0500

    Test loading angular app module

--- client/app_test.js
+++ client/app_test.js
@@ -1,5 +1,9 @@
-describe('App >', function() {
+describe('App Loading >', function() {
     it('should run tests', function() {
-        expect(true).toBe(true);
+        expect(true).toBe(true); //silly assertion that just means test runner works
     });
+});
+
+describe('Load angular and verify main module loads', function() {
+    beforeEach(module('waiterApp'));
 });
--- client/karma.conf.js
+++ client/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             'bower_components/angular/angular.min.js',
+            'bower_components/angular-mocks/angular-mocks.js',
             'app.js',
             'app_test.js'
         ],
